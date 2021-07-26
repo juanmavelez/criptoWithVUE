fd40f310eceaf61d85d758e2e9982c1423efb5e7
JuanMa Velez <juanmanuelveleztorres@gmail.com>
2021-07-26 15:08:08 +0200

    adding tailwind, adding new componenets and creating the beggining of the routes

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <main id="app"></main>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+import PxAssetsTable from "@/components/px";
+@Component({
+  components: {},
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
